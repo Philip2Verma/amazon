ab1877801c1b79746c471dfb724d94ae2d5a6204
Philip Verma <vermaphilip51@gmail.com>
2024-02-16 16:39:53 +0530

    Lesson3-Create-React-APP

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div >
+      <header>
+        <a href="/">amazon</a>
+      </header>
+      <main>
+        products list
+      </main>
+    </div>
+  );
+}
+
+export default App;
