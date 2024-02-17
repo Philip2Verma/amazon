1c27bde8c038010d9b000accbc9b40f83941ba7b
Philip Verma <vermaphilip51@gmail.com>
2024-02-17 20:50:56 +0530

    added rating

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useReducer } from "react";
-import { Link } from "react-router-dom";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import logger from "use-reducer-logger";
 import axios from "axios";
+import Product from "../components/Product";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -60,23 +62,14 @@ export default function HomeScreen() {
         ) : error ? (
           <div>{error}</div>
         ) : (
-          products.map((product) => (
-            <div className="product" key={product.slug}>
-              <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
-              <div className="products-info">
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>₹</strong>
-                  {product.price}
-                </p>
-                <button>Add to Cart</button>
-              </div>
-            </div>
-          ))
+
+          <Row>
+            {products.map((product) => (
+              <Col  key={product.slug} sm={6} md={4} lg={3} className='mb-3'>
+                <Product product={product}/>
+              </Col>
+            ))}
+          </Row>
         )}
       </div>
     </div>
